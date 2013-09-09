880528be397dc5a1eba718d98a334c6eb43e3891
Seth Helfgott <sethleland@gmail.com>
2013-09-08 22:20:39 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger2::Application.config.secret_key_base = 'afe962643d0f32af21a5872875cb10131daae1a16397013ae97f692535a95bb18fa31ba35c94e84fcafc9d9c10d45014c793a131e35da7418d3860f3df05e370'
